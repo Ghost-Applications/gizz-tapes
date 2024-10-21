6ff2bf18f13630e46a772fc31ea943d63ae0be97
Andrew Reitz <aj.reitz@gmail.com>
2024-10-22 08:13:21 +0900

    Update animations for the Full Player UI
    Player now animates up into the screen and slides down out of the screen, similar
    to other music player apps.
    
    Ticket: https://github.com/Ghost-Applications/gizz-tapes/issues/6

--- mobile/src/main/java/gizz/tapes/ui/GizzNavController.kt
+++ mobile/src/main/java/gizz/tapes/ui/GizzNavController.kt
@@ -1,8 +1,20 @@
 package gizz.tapes.ui
 
+import androidx.annotation.OptIn
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.EaseIn
+import androidx.compose.animation.core.EaseOut
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.media3.common.util.UnstableApi
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -18,12 +30,17 @@ import gizz.tapes.ui.year.YearSelectionScreen
 fun GizzNavController(
     navController: NavHostController
 ) {
-    NavHost(navController = navController, startDestination = Screen.YearSelection.route) {
-        val miniPlayerClicked = { title: Title -> navController.navigate(
-            Screen.Player.createRoute(
-                title
+    NavHost(
+        navController = navController,
+        startDestination = Screen.YearSelection.route
+    ) {
+        val miniPlayerClicked = { title: Title ->
+            navController.navigate(
+                Screen.Player.createRoute(
+                    title
+                )
             )
-        ) }
+        }
 
         composable(route = Screen.YearSelection.route) {
             YearSelectionScreen(
@@ -37,37 +54,57 @@ fun GizzNavController(
         ) {
             ShowSelectionScreen(
                 navigateUpClick = { navController.navigateUp() },
-                onShowClicked = { id, title -> navController.navigate(
-                    Screen.Show.createRoute(
-                        showId = id,
-                        title = title
+                onShowClicked = { id, title ->
+                    navController.navigate(
+                        Screen.Show.createRoute(
+                            showId = id,
+                            title = title
+                        )
                     )
-                ) },
+                },
                 onMiniPlayerClick = miniPlayerClicked
             )
         }
         composable(
             route = Screen.Show.route,
-            arguments = Screen.Show.navArguments
+            arguments = Screen.Show.navArguments,
         ) {
             ShowScreen(
                 upClick = { navController.navigateUp() },
                 onMiniPlayerClick = miniPlayerClicked
             )
         }
-        composable(
-            route = Screen.Player.route,
-            arguments = Screen.Player.navArguments
-        ) {
-            FullPlayer(
-                navigateToShow = { id, name ->
-                    navController.clearBackStack(Screen.Player.route)
-                    navController.navigate(Screen.Show.createRoute(id, name)) {
-                        popUpTo(Screen.YearSelection.route)
-                    }
-                },
-                upClick = { navController.navigateUp() },
+
+        fullPlayerNavigation(navController)
+    }
+}
+
+@OptIn(UnstableApi::class)
+fun NavGraphBuilder.fullPlayerNavigation(navController: NavHostController) {
+    composable(
+        route = Screen.Player.route,
+        arguments = Screen.Player.navArguments,
+        enterTransition = {
+            slideIntoContainer(
+                animationSpec = tween(300, easing = EaseIn),
+                towards = AnimatedContentTransitionScope.SlideDirection.Up
+            )
+        },
+        exitTransition = {
+            slideOutOfContainer(
+                animationSpec = tween(300, easing = EaseOut),
+                towards = AnimatedContentTransitionScope.SlideDirection.Down
             )
         }
+    ) {
+        FullPlayer(
+            navigateToShow = { id, name ->
+                navController.clearBackStack(Screen.Player.route)
+                navController.navigate(Screen.Show.createRoute(id, name)) {
+                    popUpTo(Screen.YearSelection.route)
+                }
+            },
+            upClick = { navController.navigateUp() },
+        )
     }
 }
