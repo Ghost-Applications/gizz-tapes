95c4f33cb99e9d2d6d7baced5b4d9510d0c53eea
Andrew Reitz <aj.reitz@gmail.com>
2025-12-17 08:00:24 -0600

    Update functions to use non deprecated month and year

--- android/src/main/java/gizz/tapes/util/localDate.kt
+++ android/src/main/java/gizz/tapes/util/localDate.kt
@@ -1,6 +1,7 @@
 package gizz.tapes.util
 
 import kotlinx.datetime.LocalDate
+import kotlinx.datetime.number
 
-fun LocalDate.toSimpleFormat(): String = "$year.$monthNumber.$dayOfMonth"
-fun LocalDate.toAlbumFormat(): String = "$year/$monthNumber/$dayOfMonth"
+fun LocalDate.toSimpleFormat(): String = "$year.${month.number}.$day"
+fun LocalDate.toAlbumFormat(): String = "$year/${month.number}/$day"
