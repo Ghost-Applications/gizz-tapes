ce3ef4cd9aabd0c13f90c5739ebe11fdc7f17f3e
Andrew Reitz <areitz@Andrews-MacBook-Pro.local>
2026-08-24 16:55:33 -0500

    Updated Libraries

--- composeApp/src/desktopMain/kotlin/gizz/tapes/DesktopAppGraph.kt
+++ composeApp/src/desktopMain/kotlin/gizz/tapes/DesktopAppGraph.kt
@@ -3,7 +3,6 @@ package gizz.tapes
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.DependencyGraph
 import dev.zacsweers.metro.Provides
-import dev.zacsweers.metro.SingleIn
 import gizz.tapes.playback.GizzMediaPlayer
 
 @DependencyGraph(scope = AppScope::class)
@@ -11,7 +10,6 @@ interface DesktopAppGraph : AppGraph {
     @DependencyGraph.Factory
     interface Factory {
         fun create(
-            @SingleIn(AppScope::class)
             @Provides appContext: AppContext
         ): DesktopAppGraph
     }
